573eb186e1f53610838707e690ede0b6d05efa12
Matheus Calixto <mcalixto2003@gmail.com>
2020-08-17 10:34:56 -0300

    inicio do desenvolvimento de um tema

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Primeira Página</title>
+    </head>
+    <body>
+        <h1> Primeira Página - Meu Tema wp </h1>
+    </body>
+</html>
